3b2a96ed1da2b585f5cdc1380e346a0120959b60
Remfath Chan <remath@gmail.com>
2017-07-22 21:03:53 +0800

    Update game funcions

--- game_functions.py
+++ game_functions.py
@@ -2,22 +2,30 @@ import sys
 import pygame
 
 
+def check_keydown_event(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.move_right = True
+    if event.key == pygame.K_LEFT:
+        ship.move_left = True
+
+
+def check_keyup_event(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.move_right = False
+    if event.key == pygame.K_LEFT:
+        ship.move_left = False
+
+
 def check_event(ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.move_right = True
-            if event.key == pygame.K_LEFT:
-                ship.move_left = True
+            check_keydown_event(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.move_right = False
-            if event.key == pygame.K_LEFT:
-                ship.move_left = False
+            check_keyup_event(event, ship)
 
 
 def update_screen(setting, screen, ship):
